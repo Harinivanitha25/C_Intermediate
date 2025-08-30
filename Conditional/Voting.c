c14a737739da32e6c424acc05be1e16501e705ae
Harini <harini.vanitha25@gmail.com>
2025-08-30 17:18:14 +0530

    Create Voting.c

---
+++ Conditional/Voting.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    scanf("%d",&age);
+    if(age>=18)
+      printf("Congratulation! You are eligible for casting your vote.");
+    else
+      printf("Sorry, You are not eligible to cast your vote.");
+}
